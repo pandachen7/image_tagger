d0969a5e9975706c513ee86a4be13d831892ece9
panda <panda.chen@asys-corp.com>
2025-09-30 13:13:18 +0800

    feat: coco to yolo

--- src/for_training/downloader.py
+++ src/for_training/downloader.py
@@ -1,10 +1,42 @@
-"""建議用另一個venv, 不然pip pkg會大洗牌"""
+"""
+建議用另一個venv, 不然pip pkg會大洗牌
+要先灌mongodb (docker可)
+記得設定
+export FIFTYONE_DATABASE_URI="mongodb://localhost:27017"
+"""
+
+import os
 
 # pip install fiftyone
+import fiftyone as fo
 import fiftyone.zoo as foz
 
+# 前面需補齊, 讓id能正確對應
+my_classes = [
+    "leo_cat",
+    "ferret",
+    "civet",
+    "cat",
+    "bicycle",
+    "bird",
+    "bus",
+    "car",
+    "dog",
+    "motorbike",
+    "person",
+    "potted plant",
+    "truck",
+]
+
+
+# 設定匯出的目標路徑
+dataset = "coco-2017"
+export_dir = f"/home/asys/datasets/img/{dataset}"
+if not os.path.exists(export_dir):
+    os.makedirs(export_dir)
+
 dataset = foz.load_zoo_dataset(
-    "coco-2017",
+    dataset,
     split="validation",
     label_types=[
         "detections",  # default
@@ -15,12 +47,12 @@ dataset = foz.load_zoo_dataset(
         "bicycle",
         "car",
         "motorcycle",
-        # "airplane",
+        "airplane",
         "bus",
-        # "train",
+        "train",
         "truck",
         # "boat",
-        "traffic light",
+        # "traffic light",
         # "fire hydrant",
         # "stop sign",
         # "parking meter",
@@ -92,5 +124,12 @@ dataset = foz.load_zoo_dataset(
         # "hair drier",
         # "toothbrush"
     ],
-    max_samples=50,
+    max_samples=2000,
+)
+
+dataset.export(
+    export_dir=export_dir,
+    dataset_type=fo.types.YOLOv5Dataset,
+    label_field="ground_truth",
+    classes=my_classes,
 )
